9f8d4b13e1785d299474a78ec17fcdd6c1582731
Victor <progfernandes@hotmail.com>
2020-08-05 18:36:31 -0300

    Meus Pedidos e Resumo Carrinho

--- site.php
+++ site.php
@@ -254,7 +254,7 @@ $app->get("/categories/:idcategory", function($idcategory){
 
 		$cart = Cart::getFromSession();
 
-		$totals = $cart->getCalculateTotal();
+		$cart->getCalculateTotal();
 
 		$order = new Order();
 
@@ -264,7 +264,7 @@ $app->get("/categories/:idcategory", function($idcategory){
 			'idaddress'=>$address->getidaddress(),
 			'iduser'=>$user->getiduser(),
 			'idstatus'=>OrderStatus::EM_ABERTO,
-			'vltotal'=>$totals['vlprice'] + $cart->getvlfreight()
+			'vltotal'=>$cart->getvltotal()
 
 		]);
 
@@ -304,6 +304,7 @@ $app->get("/boleto/:idorder", function($idorder){
 	$taxa_boleto = 5.00;
 	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
 	$valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+	$valor_cobrado = str_replace(".", "", $valor_cobrado);
 	$valor_cobrado = str_replace(",", ".",$valor_cobrado);
 	$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
@@ -359,6 +360,50 @@ $app->get("/boleto/:idorder", function($idorder){
 
 	require_once($path . "funcoes_itau.php");
 	require_once($path . "layout_itau.php");
+
+});
+
+$app->get("/profile/orders", function(){
+
+	User::verifyLogin(false);
+
+	$user = User::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders",[
+
+		'order'=>$user->getOrders()
+
+	]);
+
+});
+
+$app->get("/profile/orders/:idorder", function($idorder){
+
+	User::verifyLogin(false);
+
+	$user = User::getFromSession();
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = new Cart();
+
+	$cart->get((int)$order->getidcart());
+
+	$cart->getCalculateTotal();
+
+	$page = new Page();
+
+	$page->setTpl("profile-orders-detail",[
+
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+
+	]);
 
 });
 
