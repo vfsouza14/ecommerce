cc2888da6f54fc6fea36ef9370664756a6a86276
Victor <progfernandes@hotmail.com>
2020-08-09 19:31:44 -0300

    Admin - User - Search And Pagination

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -454,6 +454,68 @@ class User extends Model{
 
 	}
 
+	public static function getPage($page = 1, $itemsPerPage = 10){ 
+
+		/*quantidade de itens por pagina*/ 
+		$start = ($page-1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_users a 
+			INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson
+			LIMIT $start, $itemsPerPage;
+
+		"); 
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"], // quantos registros vieram
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) // quantas paginas foi geradas
+
+		];
+
+	} 
+
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10){ 
+
+		/*quantidade de itens por pagina*/ 
+		$start = ($page-1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_users a 
+			INNER JOIN tb_persons b USING(idperson)
+			WHERE b. desperson LIKE :search OR b.desemail = :search OR a.deslogin LIKE :search 
+			ORDER BY b.desperson
+			LIMIT $start, $itemsPerPage;
+
+		",[
+
+			':search'=> '%'. $search . '%'
+
+		]); 
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"], // quantos registros vieram
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) // quantas paginas foi geradas
+
+		];
+
+	} 
+
 }
 
 ?>
