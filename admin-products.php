791eab8672b5fbf305f81b1d55ce362a765421b3
Victor <progfernandes@hotmail.com>
2020-08-11 08:18:14 -0300

    Admin - Produtos - Busca e Paginação

--- admin-products.php
+++ admin-products.php
@@ -8,13 +8,47 @@ $app->get("/admin/products", function(){
 
 	User::verifyLogin();
 
-	$page = new PageAdmin();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-	$products = Products::listAll();
+	if($search != ''){
+
+		$pagination = Products::getPageSearch($search, $page);
+
+
+	} else{
+
+		$pagination = Products::getPage($page);
+
+	}
+
+	
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++){
+
+		array_push($pages, [
+
+			'href' => '/admin/products?'. http_build_query([
+
+				'page'=>$x+1,
+				'search'=>$search
+
+			]),
+
+			'text'=>$x+1
+
+		]);
+
+	}
+
+	$page = new PageAdmin();
 
 	$page->setTpl("products", [
 
-		"products"=>$products
+		"products"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
 
 	]);
 
--- vendor/hcodebr/php-classes/src/Model/Products.php
+++ vendor/hcodebr/php-classes/src/Model/Products.php
@@ -176,6 +176,68 @@ class Products extends Model{
 
 	}
 
+	public static function getPage($page = 1, $itemsPerPage = 10){ 
+
+		/*quantidade de itens por pagina*/ 
+		$start = ($page-1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+
+		"); 
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"], // quantos registros vieram
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) // quantas paginas foi geradas
+
+		];
+
+	} 
+
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10){ 
+
+		/*quantidade de itens por pagina*/ 
+		$start = ($page-1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			WHERE desproduct LIKE :search 
+			OR vlprice LIKE :search
+			ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+
+		",[
+
+			':search'=> '%'. $search . '%'
+
+		]); 
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"], // quantos registros vieram
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) // quantas paginas foi geradas
+
+		];
+
+	} 
+
+
 }
 
 
