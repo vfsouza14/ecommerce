c751b2dae8a78ab6ac7277851f7c495905fdfdaa
Victor <progfernandes@hotmail.com>
2020-08-12 18:19:55 -0300

    Integração PayPal HTML

--- site.php
+++ site.php
@@ -271,8 +271,90 @@ $app->get("/categories/:idcategory", function($idcategory){
 
 		$order->save();
 
-		header("Location: /order/". $order->getidorder());
+		switch ((int)$_POST['payment-method']) {
+
+			case 1:
+
+			header("Location: /order/". $order->getidorder()."/pagseguro");
+			break;
+
+			case 2:
+
+			header("Location: /order/". $order->getidorder()."/paypal");
+			break;
+
+		}
+
+		
 		exit;
+
+	});
+
+	$app->get("/order/:idorder/pagseguro", function($idorder){
+
+		User::verifyLogin(false);
+
+		$order = new Order();
+
+		$order->get((int)$idorder);
+
+		$cart = $order->getCart();
+
+		$productsTotals = $cart->getProductsTotals();
+
+		$freightPerProduct = number_format(($cart->getvlfreight() / $productsTotals["nrqtd"]), 2, '.', '');
+
+		$page = new Page([
+
+			'header' =>false,
+			'footer'=>false
+
+		]);
+
+		$page->setTpl("payment-pagseguro",[
+
+			'order'=>$order->getValues(),
+			'cart'=>$cart->getValues(),
+			'products'=>$cart->getProducts(),
+			"freight"=>$freightPerProduct,
+			'phone'=>[
+				'areaCode'=>substr($order->getnrphone(), 0, 2),
+				'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
+			]	
+
+		]);
+
+	});
+
+	$app->get("/order/:idorder/paypal", function($idorder){
+
+		User::verifyLogin(false);
+
+		$order = new Order();
+
+		$order->get((int)$idorder);
+
+		$cart = $order->getCart();
+
+		$productsTotals = $cart->getProductsTotals();
+
+		$freightPerProduct = number_format(($cart->getvlfreight() / $productsTotals["nrqtd"]), 2, '.', '');
+
+		$page = new Page([
+
+			'header' =>false,
+			'footer'=>false
+
+		]);
+
+		$page->setTpl("payment-paypal",[
+
+			'order'=>$order->getValues(),
+			'cart'=>$cart->getValues(),
+			'products'=>$cart->getProducts(),
+			"freight"=>$freightPerProduct
+
+		]);
 
 	});
 
